d3112c38d85a6223497440c747c5c56f996e13fb
somone-codes <someone_mails@protonmail.com>
2022-01-07 02:19:51 +0530

    Adding CORS support via middleware, opening up API for everyone via CORS.

--- src/fast_api.py
+++ src/fast_api.py
@@ -1,11 +1,23 @@
 from routers import post, user, authentication, vote
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
 # not required anymore as we are using alembic migrations
 # models.Base.metadata.create_all(bind=engine)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
